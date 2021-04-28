50291cdb6673d78fc274f3bd09b7d7f948368acf
TheGetHuber <69822538+TheGetHuber@users.noreply.github.com>
2021-04-28 21:11:31 +0300

    added aboba file

---
+++ main.py
@@ -0,0 +1,17 @@
+#its mine
+import random
+#its mine
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+#its mine
+print("Initiating aboba sequence")
+#its mine
+print("Picking random letter...")
+#its mine
+abobaletter = random.choice(letters)
+#its mine
+print("NEW ABOBA IS READY!!!")
+print("A", abobaletter, "O", abobaletter, "A")
+#its mine
+end = input("Program ended sucessfully...")
+#its mine
+#expect libraries and the python
